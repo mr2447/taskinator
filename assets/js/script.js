a6d61f376985023016608362af880b1857d5430a
Marvin Ren <marvin.ren@yale.edu>
2021-08-26 10:16:46 -0400

    code refactoring

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,13 +4,28 @@ console.log(buttonEl);
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function(event) {
+var taskFormHandler = function(event) {
     event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+    var taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    };
+    createTaskEl(taskDataObj);
+};
 
+var createTaskEl = function(taskDataObj) {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    //create div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task=type'>" + taskDataObj.type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+    //add entire list item to list
     tasksToDoEl.appendChild(listItemEl);
-};
-formEl.addEventListener("submit", createTaskHandler);
+}
+formEl.addEventListener("submit", taskFormHandler);
 
